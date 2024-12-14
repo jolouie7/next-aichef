85ad6b3725412541550a95cf7d4ab308c7fc535a
Joseph <joseph161216@gmail.com>
2024-12-13 23:30:42 -0800

    refactor: rename page component to follow naming convention
    - MealResult -> MealPage

--- app/create-meal/page.tsx
+++ app/create-meal/page.tsx
@@ -1,6 +1,6 @@
 import CreateMealForm from "@/components/create-meal-form";
 
-export default function CreateMeal() {
+export default function CreateMealPage() {
   return (
     <div className="flex flex-col container mx-auto mt-16">
       <h1 className="text-2xl tracking-tight text-zinc-900 mb-4">
