565be5a020de06bb0aff9b2bc8169ff05d79b836
Joseph <joseph161216@gmail.com>
2024-12-13 23:34:02 -0800

    feat: add meal option component

---
+++ components/meal-option.tsx
@@ -0,0 +1,40 @@
+// "use client";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface MealOptionProps {
+  title: string;
+  description: string;
+  handleClick: ({
+    title,
+    description,
+  }: {
+    title: string;
+    description: string;
+  }) => void;
+}
+
+export default function MealOption({
+  title,
+  description,
+  handleClick,
+}: MealOptionProps) {
+  return (
+    <>
+      <Card
+        className="hover:cursor-pointer hover:bg-accent"
+        onClick={() => handleClick({ title, description })}
+      >
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+      </Card>
+    </>
+  );
+}
