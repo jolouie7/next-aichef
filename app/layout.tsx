08eccc4b40b9545dd66201f0512bb62301cf480a
Joseph <joseph161216@gmail.com>
2024-12-16 22:15:50 -0800

    feat: add meal detail context

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { MainNav } from "@/components/main-nav";
 import { MealsProvider } from "@/lib/meals-context";
+import { MealDetailProvider } from "@/lib/meal-detail-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MealsProvider>
-          <MainNav />
-          {children}
+          <MealDetailProvider>
+            <MainNav />
+            {children}
+          </MealDetailProvider>
         </MealsProvider>
         <Toaster />
       </body>
---
+++ lib/meal-detail-context.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from "react";
+
+interface Meal {
+  title: string;
+  description: string;
+  // imageUrl: string;
+  ingredients: string[];
+  instructions: string[];
+}
+
+interface MealDetailContextType {
+  meal: Meal;
+  setMeal: React.Dispatch<React.SetStateAction<Meal>>;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const MealDetailContext = createContext<MealDetailContextType | null>(null);
+
+export const MealDetailProvider = ({ children }: { children: ReactNode }) => {
+  const [meal, setMeal] = useState<Meal>({
+    title: "",
+    description: "",
+    ingredients: [],
+    instructions: [],
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <MealDetailContext.Provider
+      value={{ meal, setMeal, isLoading, setIsLoading }}
+    >
+      {children}
+    </MealDetailContext.Provider>
+  );
+};
+
+export const useMealDetailContext = () => {
+  const context = useContext(MealDetailContext);
+  // Do a null check because I was getting an error saying setMeals could be null
+  if (context === null) {
+    throw new Error("There was an error getting the meal detail context");
+  }
+  return context;
+};
