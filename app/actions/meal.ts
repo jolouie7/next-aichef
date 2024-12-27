0ac39f8974dfbb3973581143c6d45f4296189f98
Joseph <joseph161216@gmail.com>
2024-12-27 14:36:16 -0800

    feat: update getAllMeals by add ingredients and instructions; add getMealById action

--- app/actions/meal.ts
+++ app/actions/meal.ts
@@ -48,11 +48,41 @@ export async function createMeal(meal: MealData) {
 // Get all meals from db
 export async function getAllMeals() {
   try {
-    const meals = await prisma.meal.findMany();
+    const meals = await prisma.meal.findMany({
+      include: {
+        ingredients: {
+          include: {
+            ingredient: true,
+          },
+        },
+        instructions: true,
+      },
+    });
     return { success: true, meals };
   } catch (error) {
     console.error("Error fetching meals:", error);
     return { success: false, meals: [] };
+  }
+}
+
+// Get meal details by id
+export async function getMealById(id: string) {
+  try {
+    const meal = await prisma.meal.findUnique({
+      where: { id },
+      include: {
+        ingredients: {
+          include: {
+            ingredient: true,
+          },
+        },
+        instructions: true,
+      },
+    });
+    return { success: true, meal };
+  } catch (error) {
+    console.error("Error fetching meal:", error);
+    return { success: false, meal: null };
   }
 }
 
