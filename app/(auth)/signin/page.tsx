49fea091e27fa0e92c164b81962a3981c15efe09
Joseph <joseph161216@gmail.com>
2025-01-04 20:00:03 -0800

    refactor: wrap Signin and Signup pages in Suspense because of useSearchParams

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
+import { Suspense } from "react";
 
 import { SigninForm } from "@/components/signin-form";
 
 export default function SigninPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10 fixed inset-0">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-10 w-10 items-center justify-center rounded-md">
-            <Image
-              src="/aichef-logo.png"
-              alt="Ai Chef Logo"
-              width={40}
-              height={40}
-            />
-          </div>
-          AI Chef
-        </a>
-        <SigninForm />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10 fixed inset-0">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="/"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="flex h-10 w-10 items-center justify-center rounded-md">
+              <Image
+                src="/aichef-logo.png"
+                alt="Ai Chef Logo"
+                width={40}
+                height={40}
+              />
+            </div>
+            AI Chef
+          </a>
+          <SigninForm />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
