1367e14e6975016dd5018ac535acb1f73eb6be94
Joseph <joseph161216@gmail.com>
2025-01-06 04:39:22 -0800

    refactor: change logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
-import { Mountain } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -33,7 +33,12 @@ export function MainNav() {
     <nav className="sticky top-0 z-50 w-full border-b bg-background/80 backdrop-blur-sm">
       <div className="container flex h-16 items-center justify-between mx-auto px-4 md:px-0">
         <Link href="/" className="flex items-center space-x-2">
-          <Mountain className="h-6 w-6" />
+          <Image
+            src="/aichef-logo.png"
+            alt="AI Chef Logo"
+            height={24}
+            width={24}
+          />
           <span className="text-lg font-semibold">AI Chef</span>
         </Link>
         <div className="ml-auto hidden md:flex md:items-center md:gap-6 mr-4">
